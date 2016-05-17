afbaec80eeed1066da5eaf28cd38d2131b69111b
zhexingli <zhexingli@gmail.com>
2016-05-17 22:24:12 +0000

    Now avoids arithmetic error when dividing by zero

--- ChooseRefFrames.py
+++ ChooseRefFrames.py
@@ -133,6 +133,9 @@ def ChooseRefFrames(red_dir,filter_id,RedConfig = None):
         for line in infile:
             if line.startswith('/'):
                 col = line.split()
+                if float(col[9]) == 0.00:
+                    float(col[9]) = 1.00
+                    
                 if (float(col[10]) < sky_limit
                     and float(col[11]) < skysigma_limit
                     and float(col[12]) < FWHM_limit
